3de717afeb9774bbac3c03df285ce7fe8288976d
ComputerVirusWannabe <94106778+ComputerVirusWannabe@users.noreply.github.com>
2021-11-11 21:24:08 +0700

    2nd commit

--- PizzaStore.java
+++ PizzaStore.java
@@ -2,6 +2,7 @@ class PizzaStore {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("WWWR");
+        System.out.println("ARGGGGG!!!\n");
     }
 
 }
